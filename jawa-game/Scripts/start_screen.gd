1500ee75b0abcf6d8e14b15571c480de08ab1eda
dbrogen <daniel.brogen1@marist.edu>
2025-10-04 13:31:44 -0400

    Created a start screen
    It has a start and credits button where the start button starts the game

---
+++ jawa-game/Scripts/start_screen.gd
@@ -0,0 +1,9 @@
+extends Control
+
+
+func _on_start_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/Exploration/desert_dunes.tscn");
+
+
+func _on_credits_pressed() -> void:
+	pass # Replace with function body.
